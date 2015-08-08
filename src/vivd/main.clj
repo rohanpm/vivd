1933851e7e27b3938a6a69dff550a0f3f43f1cad
Rohan McGovern <rohan@mcgovern.id.au>
2015-08-09 09:59:16 +1000

    Allow jetty to be configured
    Rather than hardcoding port 8080.

--- src/vivd/main.clj
+++ src/vivd/main.clj
@@ -21,16 +21,21 @@
    :default-url       "/",
    :docker-http-port  80})
 
+(defn- default-jetty-config []
+  {:port 8080
+   :max-threads 100})
+
 (defn- read-config []
   (with-open [cfg (reader-for-file "data/config.clj")]
     (edn/read cfg)))
 
 (defn -main [& args]
-  (println "vivd")
-  (let [config (read-config)
-        config (merge (default-config) config)
-        _      (log/debug "Config:" config)]
+  (let [config       (read-config)
+        config       (merge (default-config) config)
+        jetty-config (or (:jetty-config config) {})
+        jetty-config (merge (default-jetty-config) jetty-config)
+        _      (log/debug "Config:" config jetty-config)]
     (ring-jetty/run-jetty-async (-> ;(partial vivd.http_handler/handler config)
                                     (vivd.http_handler/make-handler config)
                                     reload/wrap-reload)
-                                {:port 8080})))
+                                jetty-config)))
