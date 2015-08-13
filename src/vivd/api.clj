c4a038a714b2dd57232a4a2c8151990be18da5eb
Rohan McGovern <rohan@mcgovern.id.au>
2015-08-13 21:49:02 +1000

    Make it possible to create container only if not already existing
    Include unique-git-revision=1 in query string for this behavior.
    This allows clients to avoid creating duplicate containers without
    maintaining state.
    
    Fixes #19

--- src/vivd/api.clj
+++ src/vivd/api.clj
@@ -5,7 +5,8 @@
             [clojure.java.io :as io]
             [clojure.data.json :as json]
             [clojure.tools.logging :as log]
-            clj-time.core))
+            clj-time.core
+            ring.middleware.params))
 
 (set! *warn-on-reflection* true)
 
@@ -17,22 +18,37 @@
   (with-open [reader (io/reader stream)]
     (json/read reader :key-fn keyword)))
 
-(defn- create-handler* [index request]
-  (let [body                           (:body request)
-        data                           (read-json-stream body)
-        {:keys [git-ref git-revision]} data
-        _                              (assert git-ref "Missing git-ref in request")
-        _                              (assert git-revision "Missing git-revision in request")
-        id                             (generate-id)
-        c                              {:id           id
-                                        :git-ref      git-ref
-                                        :git-revision git-revision
-                                        :status       :new
-                                        :timestamp    (clj-time.core/now)}]
+(defn- create-container [index data]
+  (let [id (generate-id)
+        c  (merge data {:id        id
+                        :status    :new
+                        :timestamp (clj-time.core/now)})]
     (index/update index c)
     (log/info "Created:" data "id:" id)
     {:status 201
      :headers {"location" (str "/" id)}}))
+
+(defn- truthy? [x]
+  (#{"1" 1 "true" true "yes"} x))
+
+(defn- lookup-container [index {:keys [git-revision]} {:keys [query-params] :or {query-params {}}}]
+  (let [unique   (query-params "unique-git-revision")
+        all-c    (index/vals index)
+        matching (filter #(= git-revision (:git-revision %)) all-c)]
+    (if (truthy? unique)
+      (if-let [{:keys [id]} (first matching)]
+        {:status  302
+         :headers {"location" (str "/" id)}}))))
+
+(defn- create-handler* [index request]
+  (let [request                                 (ring.middleware.params/assoc-query-params request "UTF-8")
+        body                                    (:body request)
+        data                                    (read-json-stream body)
+        {:keys [git-ref git-revision] :as data} (select-keys data [:git-ref :git-revision])]
+    (assert git-ref "Missing git-ref from request")
+    (assert git-revision "Missing git-revision from request")
+    (or (lookup-container index data request)
+        (create-container index data))))
 
 (defn make-create-handler [index]
   (->> (partial create-handler* index)
--- test/vivd/api_test.clj
+++ test/vivd/api_test.clj
@@ -11,18 +11,42 @@
     {:response   response
      :index-vals (index/vals index)}))
 
+(defn str-stream [str]
+  (-> (java.io.StringReader. str) clojure.lang.LineNumberingPushbackReader.))
+
 (facts "create-handler"
   (let [existing-container {:id "aBcD" :git-revision "00112233"}
-        index              (index-for [existing-container])]
-    (with-in-str "{\"git-ref\":\"foo\",\"git-revision\":\"aabbcc\"}"
-      (fact "can create new object"
-        (test-create index {:uri            "/a/container"
+        index              (fn [] (index-for [existing-container]))]
+    (fact "can create new object"
+      (test-create (index) {:uri            "/a/container"
                             :request-method :post
-                            :body           *in*})
-        => (contains {:response   (contains {:status  201
-                                             :headers (contains {"location" truthy})})
-                      :index-vals (just [existing-container
-                                         (contains {:git-ref      "foo"
-                                                    :git-revision "aabbcc"
-                                                    :status       :new})]
-                                        :in-any-order)})))))
+                            :body           (str-stream "{\"git-ref\":\"foo\",\"git-revision\":\"aabbcc\"}")})
+      => (contains {:response   (contains {:status  201
+                                           :headers (contains {"location" truthy})})
+                    :index-vals (just [existing-container
+                                       (contains {:git-ref      "foo"
+                                                  :git-revision "aabbcc"
+                                                  :status       :new})]
+                                      :in-any-order)}))
+
+    (fact "can create new object with unique-git-revision"
+      (test-create (index) {:uri            "/a/container"
+                            :query-string   "unique-git-revision=1x"
+                            :request-method :post
+                            :body           (str-stream "{\"git-ref\":\"foo\",\"git-revision\":\"aabbcc\"}")})
+      => (contains {:response   (contains {:status  201
+                                           :headers (contains {"location" truthy})})
+                    :index-vals (just [existing-container
+                                       (contains {:git-ref      "foo"
+                                                  :git-revision "aabbcc"
+                                                  :status       :new})]
+                                      :in-any-order)}))
+
+    (fact "looks up existing container by git revision"
+      (test-create (index) {:uri            "/a/container"
+                            :query-string   "foo=bar&unique-git-revision=1"
+                            :request-method :post
+                            :body           (str-stream "{\"git-ref\":\"foo\",\"git-revision\":\"00112233\"}")})
+      => (contains {:response   (contains {:status  302
+                                           :headers (contains {"location" "/aBcD"})})
+                    :index-vals (just [existing-container])}))))
