9af0afcda60fbac40dbb4f7bee3e724f9a0b7237
Rohan McGovern <rohan@mcgovern.id.au>
2015-08-13 19:58:51 +1000

    Add a test for vivd.proxy

---
+++ test/vivd/proxy_test.clj
@@ -0,0 +1,46 @@
+(ns vivd.proxy-test
+  (:require [vivd.proxy :refer [get-proxy-request]]
+            vivd.container
+            clojure.core.cache
+            [midje.sweet :refer :all]))
+
+(def test-container {:id                  "aAbBcC"
+                     :docker-container-id "abcdef"})
+
+(def test-config {:docker-http-port 123})
+
+(def test-inspect {"abcdef" {:NetworkSettings {:Ports {:123/tcp [{:HostIp   "127.0.0.2"
+                                                                   :HostPort "5566"}]}}}})
+(def test-inspect-cache
+  (atom (clojure.core.cache/basic-cache-factory test-inspect)))
+
+(def common-request-parts {:as               :stream
+                           :follow-redirects false
+                           :throw-exceptions false
+                           :scheme           :http
+                           :server-name      "127.0.0.2"
+                           :server-port      5566})
+
+(defn expected-request [r]
+  (merge common-request-parts r))
+
+(def base-request {:container-uri     "foo/bar"
+                   :container-vivd-id "aAbBcC"
+                   :remote-addr       "10.0.0.1"
+                   :headers           {"host" "example.com"}})
+
+(with-redefs [vivd.container/INSPECT-CACHE test-inspect-cache]
+  (facts "get-proxy-request"
+    (fact "returns expected values"
+      (get-proxy-request base-request test-config test-container)
+      => (expected-request {:uri "/foo/bar"
+                            :headers {"x-forwarded-for"  "10.0.0.1"
+                                      "x-forwarded-host" "example.com"}}))
+
+    (fact "passes through expected values"
+      (get-proxy-request (merge base-request {:request-method :foo
+                                              :query-string   "hi&there"
+                                              :body           "some-body"}) test-config test-container)
+      => (contains {:request-method :foo
+                    :query-string   "hi&there"
+                    :body           "some-body"}))))
