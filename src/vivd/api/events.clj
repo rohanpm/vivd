e0ae52a6ae79eccaea6b9314c29c8f1b914c3546
Rohan McGovern <rohan@mcgovern.id.au>
2015-09-27 14:06:41 +1000

    Fix broken links in server-sent events
    Remove a buggy hack which incorrectly assumed every request
    would come with the same "Host" header.
    Calculcate links accurately for each SSE endpoint.
    
    Fixes #53

--- src/vivd/api/events.clj
+++ src/vivd/api/events.clj
@@ -8,39 +8,38 @@
             [vivd.api.containers.common :refer [container-resource]]))
 
 (defn- get-events [event-channels {:keys [index]} request]
-  (sse/as-channel request
-   :on-open  (fn [ch]
-               (log/debug "sse channel opened" ch)
-               (swap! event-channels conj ch))
-   :on-error (fn [ch error]
-               ; debug, not warn/error, since broken pipe etc are normal
-               (log/debug "Error on channel" ch error))
-   :on-close (fn [ch why]
-               (log/debug "close" why)
-               (swap! event-channels disj ch))))
+  ; In order to generate events, we need to access the request
+  ; (used to generate absolute URLs).
+  ; Keep the needed subset of the request only.
+  (let [request-lite (select-keys request [:headers :scheme :server-port :server-name])]
+    (sse/as-channel request
+     :on-open  (fn [ch]
+                 (log/debug "sse channel opened" ch)
+                 (swap! event-channels assoc ch request-lite))
+     :on-error (fn [ch error]
+                 ; debug, not warn/error, since broken pipe etc are normal
+                 (log/debug "Error on channel" ch error))
+     :on-close (fn [ch why]
+                 (log/debug "close" why)
+                 (swap! event-channels dissoc ch)))))
 
-(defn- send-update [ch resource]
-  (sse/send! ch resource))
+(defn- send-update [ch resource request]
+  (let [prefix   (links/link-prefix request)
+        resource (update resource :links (partial links/prefix-link-object prefix))
+        str      (json/write-str resource)]
+    (sse/send! ch str)))
 
-(defn- send-all-updates [services channels-ref link-prefix c]
+(defn- send-all-updates [services channels-ref c]
   (log/debug "sending update" c)
   (let [channels @channels-ref
-        resource (container-resource services c)
-        resource (update resource :links (partial links/prefix-link-object @link-prefix))
-        str      (json/write-str resource)]
-    (doseq [ch channels]
-           (send-update ch str))))
+        resource (container-resource services c)]
+    (doseq [[ch request] (seq channels)]
+           (send-update ch resource request))))
 
 (defn make [config {:keys [index] :as services}]
-  (let [event-channels (atom #{})
-        ; bit of a hack.  we need a request to figure out prefix for absolute links,
-        ; but doesn't matter which one. So we just use the first one for that.
-        link-prefix (atom nil)]
-    (index/watch-update index (partial send-all-updates services event-channels link-prefix))
+  (let [event-channels (atom {})]
+    (index/watch-update index (partial send-all-updates services event-channels))
     (fn [request]
-      (swap! link-prefix
-             (fn [val]
-               (or val (links/link-prefix request))))
       (routing request
                (GET "/a/events" [:as r]
                     (get-events event-channels services r))))))
